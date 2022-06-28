3bd06db3b23260bfeda2b61f546ebcdf5057c258
monicaliz <monicaeliz3@gmail.com>
2022-06-27 21:49:06 -0400

    commit tree a27815af4106d370955434585de532b07bae0c08

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+//LAB INSTRUCTIONS
+
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+                name,
+                email
+
+        })
+
+    })
+    .then( response => response.json() )
+    .then( response => {
+            document.body.innerHTML = `<p>${response.id}</p>`
+    })
+    .catch(error => {
+        document.body.innerHTML = `<p>${error.message}</p>`
+    })
+}
